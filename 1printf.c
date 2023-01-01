542f106a76750b5a49d0bf1760b56d5d5f618b7a
nandinihirpara <119646455+nandinihirpara@users.noreply.github.com>
2023-01-01 12:06:46 +0530

    Add files via upload

---
+++ 1printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+// using printf function.
+{
+    printf("my name is Nandini\n");
+    printf("my birth date is:07/04/1999\n");
+    printf("my age is= 23 year\n");
+    printf("my addres is = G-5,Shiv apparetment,minibajar,varachha,SURAT-06.");
+    return 0;
+}
